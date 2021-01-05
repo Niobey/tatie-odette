a038a54f264a310edd1c0efc1ad75a13c17f19a6
Léo O <74404063+Leo-newbie@users.noreply.github.com>
2021-01-05 17:04:41 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import VueRouter from 'vue-router'
 import Contact from '../views/Contact.vue'
 import GeneralInformations from '../views/GeneralInformations.vue'
 import Home from '../views/Home.vue'
-import Map from '../views/Map.vue'
-import Programmation from "../views/Programmation.vue"
 import LegalNotice from '../views/LegalNotice.vue'
 import Map from '../views/Map.vue'
 import MeetArtists from '../views/MeetArtists.vue'
