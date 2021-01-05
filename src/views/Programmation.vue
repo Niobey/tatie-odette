9057780aaebd3e3a024dee390f982a283ed0b89d
Léo O <74404063+Leo-newbie@users.noreply.github.com>
2021-01-05 18:03:17 +0100

    Update Programmation.vue

--- src/views/Programmation.vue
+++ src/views/Programmation.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="programmation">
-    <h1>Programmation</h1>
     <div class="container">
       <div class="card">
         <div class="card_title">
@@ -14,56 +13,43 @@
 </template>
 
 <style lang="scss">
-.container{
-  margin-top: 10em;
-}
-
 .card {
-  margin: auto;
-  width: 80%;
+  width: 100%;
   height: 140px;
   margin-bottom: 10px;
   font-family: Helvetica;
-
 }
-
   .card_title{
     width: auto;
     background: #FCF5BE;
     display: flex;
     border-bottom: 2px solid #FF8E4E;
-    padding-top: .4em;
   }
-
   .card_title h3{
 width: 92px;
-margin: 0;
-
+height: 20px;
+left: 22px;
+top: 11em;
 font-family: Raleway;
 font-style: normal;
 font-weight: 800;
 font-size: 12px;
-
+line-height: 20px;
 color: #000000;
   }
-
   .card_title span{
 width: 188px;
 height: 18px;
 left: 161px;
 top: 181px;
-
 font-family: Raleway;
 font-style: normal;
 font-weight: normal;
 font-size: 12px;
 line-height: 14px;
 text-align: right;
-margin-left: auto;
-
 color: #000000;
   }
-
 img{
 margin-top: .5em;
 width: 100%;
