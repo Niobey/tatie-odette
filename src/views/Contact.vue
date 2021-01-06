4b6e7440c214c9dce83b9a3b38c7da716da3a9ff
Gohon95 <gohon95@gmail.com>
2021-01-06 16:16:09 +0100

    ajout page contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Contact</h1>
+    <div class="text">
+      <h2>Qui sommes-nous ?</h2>
+      <p>« Live Events » est une entreprise
+      </p>
+      <p>d’organisation et de promotion de spectacles basée sur Paris et créée en 2005.</p>
+      <p>Elle compte aujourd’hui une centaine
+      d’employés.</p>
+    </div>
+    <div class="presentation">
+      <h2>Axel Gonon</h2>
+      <img class="image" src="../assets/img/axel.jpg" alt="axel"/>
+    </div>
   </div>
 </template>
+
+
+<style lang="scss">
+.image {
+  margin-left: 1px;
+   width:100px;
+   height: 100px;
+   clip-path:ellipse(50% 50%);
+}
+
+.presentation {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 700px;
+}
+
+.text {
+  margin-left: 75px;
+  margin-top: 60px;
+}
+</style>
+
