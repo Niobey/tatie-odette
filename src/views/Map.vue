c7bf6ad1219e3208ed4f1bd61ddbeaefef3db803
Violette <violette.hurel@gmail.com>
2021-01-10 23:48:01 +0100

    Create custom markers WIP issues import img

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -4,30 +4,75 @@
     <l-map
       :zoom="zoom"
       :center="center"
+      :enableHighAccuracy="enableHighAccuracy"
       @ready="onReady"
       @locationfound="onLocationFound"
       class="map"
+      v-for="(marker, index) in markers"
+      :key="index"
     >
-      <l-tile-layer :url="url" :attribution="attribution" />
+      <l-marker :lat-lng="setLatLng(marker.lat, marker.lng)">
+        <l-icon :icon-anchor="staticAnchor">
+          <!-- <ArrowMarker :class="marker.type" /> -->
+          <img :src="setIcon(marker.type)" />
+          <Stage style="height: 35px; transform: translate(-50%, -50%);"/>
+        </l-icon>
+      </l-marker>
+
+      <l-tile-layer :url="url" :attribution="attribution" :compass="compass" />
       <l-geo-json v-if="show" :geojson="geojson" />
 
-      <l-marker :lat-lng="marker">
+      <l-marker :lat-lng="user">
         <l-icon :icon-anchor="staticAnchor" class-name="arrow-marker">
           <ArrowMarker class="arrow-marker" />
         </l-icon>
       </l-marker>
+      <l-geo-json :geojson="geojson" :options-style="styleGeo"></l-geo-json>
+      <l-control position="bottomleft">
+        <button>
+          I am a useless button!
+        </button>
+      </l-control>
     </l-map>
   </div>
 </template>
 
 <script>
+import Stage from "../assets/markers/stage.svg";
 import Plan from "../components/Plan.vue";
 import ArrowMarker from "../assets/markers/marker-arrow.svg";
-import { latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker, LIcon, LGeoJson } from "vue2-leaflet";
-// Geo Json sont des coordonnées pour ajouter un svg sur la map
+import { latLng, Control } from "leaflet";
+import Compass from "../../node_modules/leaflet-compass/src/leaflet-compass";
+
+// https://leafletjs.com/reference-1.7.1.html#control -> Heading (ctrl + D : North)
+// https://stackoverflow.com/questions/28396206/google-maps-js-api-v3-show-orientation-arrow-on-my-location-marker
+// https://mobiforge.com/design-development/html5-mobile-web-device-orientation-events
 
 // https://www.npmjs.com/package/vue2-leaflet-rotatedmarker
+
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LIcon,
+  LGeoJson,
+  LControl,
+} from "vue2-leaflet";
+// Geo Json sont des coordonnées pour ajouter un svg sur la map
+
+const poiIcons = {
+  stage: "../assets/markers/stage.svg",
+  user: "..assets/markers/marker-arrow.svg",
+};
+
+const poi = [
+  {
+    name: "Scène 1",
+    lat: 47.206557,
+    lng: -1.54021,
+    type: "stage",
+  },
+];
 
 export default {
   name: "Map",
@@ -38,26 +83,30 @@ export default {
     LMarker,
     LIcon,
     ArrowMarker,
+    LControl,
+    Stage
   },
   data() {
     return {
       loading: false,
       show: true,
       enableTooltip: true,
-      zoom: 15,
-      center: [47.239543, -1.510883],
+      zoom: 17,
+      center: [47.205584, -1.540117],
       geojson: null,
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      // marker: latLng(48.944021, 2.394466),
-      marker: latLng(47.2381374, -1.5095204),
-      // icon: L.icon({
-      //   iconUrl: "../assets/markers/marker-arrow.png",
-      //   iconSize: [32, 37],
-      //   iconAnchor: [16, 37],
-      // }),
+      user: latLng(47.20558, -1.538199),
       staticAnchor: [16, 37],
+      enableHighAccuracy: true,
+      compass: Compass,
+      styleGeo: {
+        fillColor: "#d0f4de",
+        fillOpacity: 1,
+      },
+      markers: poi,
+      icons: poiIcons,
     };
   },
   methods: {
@@ -67,11 +116,34 @@ export default {
     onLocationFound(location) {
       // https://github.com/vue-leaflet/Vue2Leaflet/issues/476
       this.marker = latLng(location.latitude, location.longitude);
-      // console.log(location.latitude, location.longitude);
-      //     navigator.geolocation.getCurrentPosition(function(position) {
-      //   console.log(position.coords.latitude, position.coords.longitude);
-      // });
     },
+    setLatLng(lat, lng) {
+      return latLng(lat, lng);
+    },
+    setIcon(type) {
+      for (const [key, value] of Object.entries(poiIcons)) {
+        if (key === type) {
+          console.log(value);
+          return value;
+        }
+      }
+    },
+  },
+  mounted() {
+    this.compass = new L.Control.Compass({ autoActive: true, showDigit: true });
+    // Ajouter le controleur Compass
+  },
+  async created() {
+    this.loading = true;
+    const response = await fetch(`./festival.geojson`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const data = await response.json();
+    this.geojson = data;
+    this.loading = false;
   },
 };
 </script>
@@ -92,5 +164,10 @@ export default {
 
 .arrow-marker {
   height: 30px;
+}
+
+.leaflet-interactive {
+  stroke: var(--orange);
+  stroke-linejoin: round;
 }
 </style>
