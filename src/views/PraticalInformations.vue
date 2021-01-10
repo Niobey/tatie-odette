e889e1c7ca7e83c167c7f5806791905dad420e03
Léo O <74404063+Leo-newbie@users.noreply.github.com>
2021-01-10 18:26:55 +0100

    Revert "Merge branch 'main' of https://github.com/Alanrl22/nation-sound-festival into main"
    This reverts commit 0a61ed6255f8db62ea07b9d290b5587032e94485, reversing
    changes made to 44ac5f97df3edc1e8a7a02eab18379daa327b499.

--- src/views/PraticalInformations.vue
+++ src/views/PraticalInformations.vue
@@ -5,52 +5,10 @@ import VueSimpleAccordion from 'vue-simple-accordion';
   <div class="PraticalInformations">
     <h1>Informations pratiques et FAQ</h1>
     
-    <h2 class="titlefind">Venir</h2>
+    <h2 class="titlefind">Nous trouver</h2>
 
-  <p class="adress">Le festival est situé Parc du Port-Mulon à Nort-sur-Erdre (44 390) en Loire-Atlantique à environ 25 km au nord de Nantes.</p>
+  <p class="adress">Adresse et autres infos pratiques.</p>
 
-  <div class="maps">
-    <div>
-      <span v-if="loading">Loading...</span>
-      <!-- <label for="checkbox">GeoJSON Visibility</label>
-      <input
-        id="checkbox"
-        v-model="show"
-        type="checkbox"
-      >
-      <label for="checkboxTooltip">Enable tooltip</label>
-      <input
-        id="checkboxTooltip"
-        v-model="enableTooltip"
-        type="checkbox"
-      >
-      <input
-        v-model="fillColor"
-        type="color"
-      > -->
-      <br>
-    </div>
-    <l-map
-      :zoom="zoom"
-      :center="center"
-      style="height: 200px; width: 70%; margin:15%"
-    >
-      <l-tile-layer
-        :url="url"
-        :attribution="attribution"
-      />
-      <l-geo-json
-        v-if="show"
-        :geojson="geojson"
-        :options="options"
-        :options-style="styleFunction"
-      />
-      <l-marker :lat-lng="marker" />
-    </l-map>
-  </div>
-
-<div class="faq">
-  <h2 class="titlefaq">FAQ</h2>
 <div class="PraticalInformationsArcordion">
         <div :key="index" v-for="(item,index) in questions" role="tablist">
             <b-card no-body class="mb-1">
@@ -65,31 +23,29 @@ import VueSimpleAccordion from 'vue-simple-accordion';
             </b-card>
         </div>  
     </div>
-</div>
+
 
   </div>
 </template>
 
 <style lang="scss">
 .PraticalInformations{
-padding-bottom: 15%;
+
 .btn-info{
-   background-color: var(--yellow)!important ; 
-   border-color: var(--yellow)!important;
+   background-color: var(--yellow) ; 
+   border-color: var(--yellow);
    font-weight: 700;
    color:black;
 }
 
 .btn-info:hover {
-  background-color: var(--blue)!important ; 
-   border-color: var(--blue)!important;
+  background-color: var(--blue) ; 
+   border-color: var(--blue);
    font-weight: 700;
    color:black;
 }
 .adress{
-  text-align: justify;
-  margin-left:1.5em;
-  margin-right:1.5em;
+  text-align: center;
 }
 
 .PraticalInformationsArcordion{
@@ -101,42 +57,9 @@ margin-top: 8.5vh;
 }
 
 h2.titlefind{
-  text-align: left;
-  margin-top: 1.5em;
-  margin-left:1.5em;
+  text-align: center;
+  margin-top: 1.5em
 }
-
-p.adress{
-  display: flex;
-  float: left;
-}
-
-.maps l-map{
-  margin-left: 20%;
-}
-
-h2.titlefaq{
-  text-align: left;
-  margin-top: 1.5em;
-  margin-left:1.5em;
-}
-
-.PraticalInformationsArcordion{
-    margin-left:1.5em;
-    margin-right:1.5em;
-    background: none;
-}
-
-.card-header {
-background-color: none!important;
-border: none!important;
-}
-.card{
-  border: none;
-}
-
-
-
 } 
 </style>
 
@@ -144,34 +67,10 @@ border: none!important;
 
 
 <script>
-import { latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker, LGeoJson } from "vue2-leaflet";
-
 export default {
-  name: "Example",
-  components: {
-    LMap,
-    LTileLayer,
-    LGeoJson,
-    LMarker
-
-
-  },
-  data() {
-    return {
-      loading: false,
-      show: false,
-      enableTooltip: true,
-      zoom: 5,
-      center: [48, -1.219482],
-      geojson: null,
-      fillColor: "#e4ce7f",
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: latLng(47.20638734052255, -1.5393574567593957),
-    
-      questions:[
+    data: function(){
+        return{
+            questions:[
                 {
                     title: 'Comment devenir Bénévole ?',
                     content: 'Lorem Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terryrichardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.'
@@ -194,55 +93,9 @@ export default {
                 },
                 
             ]
-
-  }
-  },
-
-  computed: {
-    options() {
-      return {
-        onEachFeature: this.onEachFeatureFunction
-      };
-    },
-    styleFunction() {
-      const fillColor = this.fillColor; // important! need touch fillColor in computed for re-calculate when change fillColor
-      return () => {
-        return {
-          weight: 2,
-          color: "#ECEFF1",
-          opacity: 1,
-          fillColor: fillColor,
-          fillOpacity: 1
-        };
-      };
-    },
-    onEachFeatureFunction() {
-      if (!this.enableTooltip) {
-        return () => {};
-      }
-      return (feature, layer) => {
-        layer.bindTooltip(
-          "<div>code:" +
-            feature.properties.code +
-            "</div><div>nom: " +
-            feature.properties.nom +
-            "</div>",
-          { permanent: false, sticky: true }
-        );
-      };
+        }
     }
-  },
-  async created() {
-    this.loading = true;
-    const response = await fetch("https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson")
-    const data = await response.json();
-    this.geojson = data;
-    this.loading = false;
   }
-
-
-};
-
 
 
 </script>
--- src/views/Programmation.vue
+++ src/views/Programmation.vue
@@ -1,98 +1,18 @@
 <template>
   <div class="programmation">
     <h1>Programmation</h1>
-    <b-row align-h="center " id="brow">
-      <div>
-        <b-dropdown
-          text="Jour"
-          variant="outline-danger"
-          class="s-1"
-          id="button-programmation"
-        >
-          <b-dropdown-item value="All" @click="selectedType = 'All'">
-            <strong>Tous</strong>
-          </b-dropdown-item>
-          <b-dropdown-item
-            v-for="day in days"
-            :key="day"
-            :value="day"
-            @click="selectedType = day"
-          >
-            {{ day }}
-          </b-dropdown-item>
-        </b-dropdown>
-      </div>
-      <div>
-        <b-dropdown
-          text="Heure"
-          variant="outline-danger"
-          class="s-1"
-          id="button-programmation"
-        >
-          <b-dropdown-item value="All" @click="selectedType = 'All'">
-          </b-dropdown-item>
-          <b-dropdown-item
-            v-for="hour in hours"
-            :key="hour"
-            :value="hour"
-            @click="selectedType = hour"
-          >
-            {{ hour }}
-          </b-dropdown-item>
-        </b-dropdown>
-      </div>
-
-      <div>
-        <b-dropdown
-          text="Genre"
-          variant="outline-danger"
-          class="s-1"
-          id="button-programmation"
-        >
-          <b-dropdown-item value="All" @click="selectedType = 'All'">
-            <strong>Tous</strong>
-          </b-dropdown-item>
-          <b-dropdown-item
-            v-for="type in types"
-            :key="type"
-            :value="type"
-            @click="selectedType = type"
-          >
-            {{ type }}
-          </b-dropdown-item>
-        </b-dropdown>
-      </div>
-      <div>
-        <b-dropdown
-          text="Scène"
-          variant="outline-danger"
-          class="s-1"
-          id="button-programmation"
-        >
-          <b-dropdown-item value="All" @click="selectedType = 'All'">
-            <strong>Tous</strong>
-          </b-dropdown-item>
-          <b-dropdown-item
-            v-for="stage in stages"
-            :key="stage"
-            :value="stage"
-            @click="selectedType = stage"
-          >
-            {{ stage }}
-          </b-dropdown-item>
-        </b-dropdown>
-      </div>
-    </b-row>
-
-    <div class="container-fluid d-flex flex-wrap">
-      <div
-        class="card_prog col-md-6 col-xl-4"
-        v-for="(card, index) in filteredCategory"
-        :key="index"
-      >
+    <div>
+      <b-dropdown text="Genre" variant="outline-danger" class="m-2">
+        <b-dropdown-item href="#" v-for="card in cards" :key="card" >
+          {{ card.type }}
+        </b-dropdown-item>
+      </b-dropdown>
+    </div>
+    <div class="container">
+      <div class="card_prog" v-for="(card, index) in cards" :key="index">
         <div class="card_title">
-          <h3>{{ card.artist }}</h3>
-          <span> {{ card.stage }} / {{ card.day }} / {{ card.hour }} </span>
+          <h3>{{ card.artiste }}</h3>
+          <span> {{ card.scene }} / {{ card.jour }} / {{ card.heure }} </span>
         </div>
         <div class="container_img">
           <img class="card_img" :src="card.img" />
@@ -101,11 +21,10 @@
     </div>
   </div>
 </template>
-/***************************************************JS******************************************************************************
-*/
+/***************************************************JS****************************************************************************** */
 
 <script>
-let types = [
+let type = [
   "rap",
   "rock",
   "metal",
@@ -115,7 +34,8 @@ let types = [
   "jazz",
   "alternatif",
 ];
-let artist = [
+
+let artiste = [
   "Booba",
   "Vald",
   "Petit Biscuit",
@@ -129,159 +49,135 @@ let artist = [
   "Tragédie",
   "Keen v",
 ];
-let stage = ["Scène A", "Scène B", "Scène C"];
-let hour = ["18h", "19h", "20h", "21h", "22h", "23h", "00h", "01h00"];
-let day = ["Vendredi", "Samedi", "Dimanche"];
+
+let scene = ["Scène A", "Scène B", "Scène C"];
+
+let heure = ["18h", "19h", "20h", "21h", "22h", "23h", "00h", "01h00"];
+
+let jour = ["Vendredi", "Samedi", "Dimanche"];
+
 const cardsData = [
   {
-    artist: artist[2],
-    type: types[0],
+    artiste: artiste[2],
+    type: type[0],
     img:
       "https://media.virginradio.fr/article-4260166-head-f8/petit-biscuit.jpg",
-    stage: stage[0],
-    day: day[2],
-    hour: hour[4],
+    scene: scene[0],
+    jour: jour[2],
+    heure: heure[4],
   },
   {
-    artist: artist[0],
-    type: types[1],
+    artiste: artiste[0],
+    type: type[1],
     img:
       "https://www.parisladefense-arena.com/uploads/2018/10/3764-booba-orig-2.jpg",
-    stage: stage[1],
-    day: day[0],
-    hour: hour[5],
+    scene: scene[1],
+    jour: jour[0],
+    heure: heure[5],
   },
   {
-    artist: artist[1],
-    type: types[2],
+    artiste: artiste[1],
+    type: type[2],
     img:
       "https://cdn.radiofrance.fr/s3/cruiser-production/2019/04/05e9523a-428f-4798-ad07-c4abcc70acfa/801x410_vald_album_2019.jpg",
-    stage: stage[2],
-    day: day[1],
-    hour: hour[5],
+    scene: scene[2],
+    jour: jour[1],
+    heure: heure[5],
   },
   {
-    artist: artist[5],
-    type: types[0],
+    artiste: artiste[5],
     img:
       "https://static1.purepeople.com/articles/6/30/54/86/@/4322687-semi-exclusif-jean-schultheis-vendre-950x0-1.jpg",
-    stage: stage[0],
-    day: day[1],
-    hour: hour[6],
+    scene: scene[0],
+    jour: jour[1],
+    heure: heure[6],
   },
 ];
+
 export default {
   data() {
-    return {
-      cards: cardsData,
-      types: types,
-      days: day,
-      hours: hour,
-      stages: stage,
-      selectedType: "All",
-    };
+    selectedType: ''; 
+    return { 
+      cards: cardsData, 
+      types: type,
+      selectedType: "" };
   },
   computed: {
-    filteredCategory: function () {
-      let self = this;
-      let cardsArray = self.cards;
-      let selectedType = self.selectedType;
-      if (selectedType === "All") {
-        return cardsArray;
-      } else {
-        cardsArray = cardsArray.filter(function (card) {
-          if (
-            card.type.indexOf(selectedType) !== -1 ||
-            card.day.indexOf(selectedType) !== -1 ||
-            card.hour.indexOf(selectedType) !== -1 ||
-            card.stage.indexOf(selectedType) !== -1
-          ) {
-            return card;
-          }
-        });
-      }
-      return cardsArray;
-    },
-  },
+		filteredType: function() {
+			var self = this;
+			var category = self.selectedType;
+			
+			if(category === "") {
+				console.log (self.cards);
+			} else {
+        console.log(ok) 
+        // self.cards.filter(function(type) {
+					//return artiste.category === category;
+				//});
+			}
+		}
+}
 };
 </script>
 
-/***************************************************CSS******************************************************************************
-*/
+/***************************************************CSS****************************************************************************** */
 
 <style lang="scss">
-#brow {
-  margin-top: 2em;
-  margin-right: auto;
-  margin-left: auto;
-}
-
-.btn {
-    display: inline-block;
-    font-weight: 400;
-    color: var(--black) !important;
-    text-align: center;
-    vertical-align: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    background-color: var(--green) !important;
-    border: 1px solid black !important;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    border-radius: 2em !important;
-    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
-
-
-
-#button-programmation {
-    background: var(--green);
-    margin: .2em;
-    border-radius: 1em ;
-    border-color: var(--blue);
-}
-
 .container {
   margin-top: 10em;
 }
+
 .card_prog {
-  margin: 2em auto auto;
-  width: 100%;
+  margin: 2em auto auto auto;
+  width: 80%;
+  height: 140px;
+  font-family: Helvetica;
 }
+
 .card_title {
-  background: var(--yellow);
-  border-bottom: 2px solid var(--orange);
+  width: auto;
+  background: #fcf5be;
   display: flex;
+  border-bottom: 2px solid #ff8e4e;
   padding-top: 0.4em;
-  width: 100%;
-  h3 {
-    font-weight: 800;
-    font-size: 12px;
-    margin: 0;
-  }
-  span {
-    font-size: 12px;
-    height: 18px;
-    left: 161px;
-    line-height: 14px;
-    margin-left: auto;
-    text-align: right;
-    top: 181px;
-  }
 }
+
+.card_title h3 {
+  width: 92px;
+  margin: 0;
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 12px;
+  color: #000000;
+}
+
+.card_title span {
+  width: 188px;
+  height: 18px;
+  left: 161px;
+  top: 181px;
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 14px;
+  text-align: right;
+  margin-left: auto;
+  color: #000000;
+}
+
 .container_img {
-  border: 2px solid var(--blue);
-  height: 10em;
-  margin-top: 0.5em;
+  height: 9em;
   overflow: hidden;
+  bottom: 9em;
 }
+
 .card_img {
-  height: 100%;
-  object-fit: cover;
-  object-position: top;
+  margin-top: 0.5em;
   width: 100%;
+  height: 90%;
+  border: 2px solid var(--blue);
 }
 </style>
+
