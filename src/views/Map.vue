70908c1b0bcf667eedf1ab1b1419f4680e7e758e
Violette <violette.hurel@gmail.com>
2021-01-10 13:41:31 +0100

    Custom marker

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="map-container">
     <!-- <Plan /> -->
-    <!-- <div>
-      <span v-if="loading">Loading...</span>
-      <label for="checkbox">GeoJSON Visibility</label>
-      <input id="checkbox" v-model="show" type="checkbox" />
-      <label for="checkboxTooltip">Enable tooltip</label>
-      <input id="checkboxTooltip" v-model="enableTooltip" type="checkbox" />
-      <input v-model="fillColor" type="color" />
-      <br />
-    </div> -->
-    <l-map :zoom="zoom" :center="center" class="map">
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      @ready="onReady"
+      @locationfound="onLocationFound"
+      class="map"
+    >
       <l-tile-layer :url="url" :attribution="attribution" />
-      <l-geo-json
-        v-if="show"
-        :geojson="geojson"
-      />
-      <l-marker :lat-lng="marker" />
+      <l-geo-json v-if="show" :geojson="geojson" />
+
+      <l-marker :lat-lng="marker">
+        <l-icon :icon-anchor="staticAnchor" class-name="arrow-marker">
+          <ArrowMarker class="arrow-marker" />
+        </l-icon>
+      </l-marker>
     </l-map>
   </div>
 </template>
 
 <script>
 import Plan from "../components/Plan.vue";
+import ArrowMarker from "../assets/markers/marker-arrow.svg";
 import { latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker, LGeoJson } from "vue2-leaflet";
+import { LMap, LTileLayer, LMarker, LIcon, LGeoJson } from "vue2-leaflet";
+// Geo Json sont des coordonnées pour ajouter un svg sur la map
 
 export default {
   name: "Map",
@@ -33,21 +34,41 @@ export default {
     LTileLayer,
     LGeoJson,
     LMarker,
+    LIcon,
+    ArrowMarker,
   },
   data() {
     return {
       loading: false,
       show: true,
       enableTooltip: true,
-      zoom: 17,
+      zoom: 15,
       center: [47.239543, -1.510883],
       geojson: null,
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       // marker: latLng(48.944021, 2.394466),
-      marker: latLng(47.2381374,-1.5095204),
+      marker: latLng(47.2381374, -1.5095204),
+      // icon: L.icon({
+      //   iconUrl: "../assets/markers/marker-arrow.png",
+      //   iconSize: [32, 37],
+      //   iconAnchor: [16, 37],
+      // }),
+      staticAnchor: [16, 37],
     };
+  },
+  methods: {
+    onReady(mapObject) {
+      mapObject.locate();
+    },
+    onLocationFound(location) {
+      // https://github.com/vue-leaflet/Vue2Leaflet/issues/476
+      this.marker = latLng(location.latitude, location.longitude);
+    },
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //   faireQqc(position.coords.latitude, position.coords.longitude);
+    // });
   },
 };
 </script>
@@ -64,5 +85,9 @@ export default {
   max-height: 500px;
   width: 50%;
   max-width: 800px;
+}
+
+.arrow-marker {
+  height: 30px;
 }
 </style>
