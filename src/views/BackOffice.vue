800ec89832c8d62d2a3a980199f498c8f3b623dc
Violette <violette.hurel@gmail.com>
2021-01-12 00:10:42 +0100

    Set min-width to buttons and other css

--- src/views/BackOffice.vue
+++ src/views/BackOffice.vue
@@ -3,37 +3,38 @@
     <div class="header-bo">
       <Logo id="home-logo" title="Festival Nation Sounds" />
     </div>
-    <container class="container-bo">
-        <h2 class="title">Cliquer sur un boutton pour modifier une page</h2>
-      <router-link class="begin" to="/programmation-administer">
+    <div class="container-bo">
+      <h2 class="title">BackOffice</h2>
+      <p>Cliquez sur un bouton pour modifier une page</p>
+      <router-link to="/programmation-administer">
         <button>Programmation</button>
       </router-link>
-      <router-link class="begin" to="/ticket-administer">
-        <button>Billeterie</button>
+      <router-link to="/ticket-administer">
+        <button>Billetterie</button>
       </router-link>
-      <router-link class="begin" to="/faq-actuality-administer">
+      <router-link to="/faq-actuality-administer">
         <button>Informations Pratiques & FAQ</button>
       </router-link>
-      <router-link class="begin" to="/plan-administer">
+      <router-link to="/plan-administer">
         <button>Plan du festival</button>
       </router-link>
-      <router-link class="begin" to="/general-informations-administer">
+      <router-link to="/general-informations-administer">
         <button>Informations Générales</button>
       </router-link>
-      <router-link class="begin" to="/meet-artists-administer">
-        <button>Rencontre les artistes</button>
+      <router-link to="/meet-artists-administer">
+        <button>Rencontrer les artistes</button>
       </router-link>
-      <router-link class="begin" to="/contact-administer">
+      <router-link to="/contact-administer">
         <button>Contact</button>
       </router-link>
-      <router-link class="begin" to="/legal-notice-administer">
+      <router-link to="/legal-notice-administer">
         <button>Mentions Légales</button>
       </router-link>
-    </container>
+    </div>
   </div>
 </template>
 
-    <script>
+<script>
 import Logo from "../assets/logo.svg";
 export default {
   components: {
@@ -42,34 +43,46 @@ export default {
 };
 </script>
 
-    <<style lang="scss">
+<<style lang="scss">
+.backoffice {
+  .header-bo {
+    background: var(--orange);
+    height: 7rem;
+  }
 
-    .header-bo{
-        background: var(--orange);
-        height: 7rem;
-    }
+  #home-logo {
+    margin: 1rem 1rem 4rem 8rem;
+    width: 8rem;
+    z-index: 1;
+  }
 
-        #home-logo {
-  margin: 1rem 1rem 4rem 8rem;
-  width: 8rem;
-  z-index: 1;
+  .container-bo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .title {
+    margin-top: 4vh;
+    font-weight: bold;
+    font-size: 2em;
+  }
+
+  p {
+    margin-bottom: 4vh;
+  }
+
+  button {
+    background: white;
+    border-radius: 50px;
+    border: 1px solid black;
+    box-shadow: -2px 5px 0 var(--blue);
+    cursor: pointer;
+    font-weight: 700;
+    margin-top: 0.7em;
+    margin-bottom: 0.3rem;
+    min-width: 25rem;
+    padding: 1em 4em;
+  }
 }
-
-    .container-bo{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    .title{
-        margin-top: 1rem;
-        font-weight: 400;
-    }
-
-    .begin button {
-
-        margin-top: .7em;
-        margin-bottom : .3rem;
-    }
-    </style>
-
+</style>
