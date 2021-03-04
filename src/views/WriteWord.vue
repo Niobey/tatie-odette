f779d23300a9e89606a534d4b61f9699cfcc1c15
Niobey <61284883+Niobey@users.noreply.github.com>
2021-03-04 16:25:43 +0100

    keyboard non responsive

--- src/views/WriteWord.vue
+++ src/views/WriteWord.vue
@@ -1,15 +1,77 @@
 <template>
-  <div class="writeWord">
-    <h1>Votre texte</h1>
+
+
+<div id="app">
+  <h1>Votre message</h1>
+    <input
+      :value="input"
+      class="input message"
+      @input="onInputChange"
+      placeholder="Utilisez le clavier pour écrire votre message"
+    >
+    <SimpleKeyboard @onChange="onChange" @onKeyPress="onKeyPress" :input="input"/>
   </div>
 </template>
 
+<script>
+import SimpleKeyboard from "./SimpleKeyboard";
+
+
+export default {
+  name: "App",
+  components: {
+    SimpleKeyboard
+  },
+  data: () => ({
+    input: ""
+  }),
+  methods: {
+    onChange(input) {
+      this.input = input;
+    },
+    onKeyPress(button) {
+      console.log("button", button);
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    }
+  }
+};
+</script>
 
 <style lang="scss">
-</style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-<script>
-export default {
-  
+
+  h1{
+  text-align: center;
+  color: #451E10 ;
+  margin-top: 6vh;
+  margin-bottom: 8vh;
+  font-size: 1.75rem;
+  text-align: center;
+
 }
-</script>
+
+  input.message {
+  width: 50vw;
+  text-align: center; 
+  padding: 5px;
+  font-size: 18px;
+  border: none;
+  box-sizing: border-box;
+  margin-left: 25%;
+  margin-bottom: 18vh;
+
+
+  }
+.simple-keyboard {
+  max-width: 850px;
+  text-align: center;
+   margin-left: 15%;
+}
+}
+</style>
